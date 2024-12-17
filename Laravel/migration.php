1170e83ca80a7da21bee14b0f4020e92d7d1ec88
tailoi <tailoi16063003@gmail.com>
2024-12-17 22:53:33 +0700

    tai nguyen
    tai

---
+++ Laravel/migration.php
@@ -0,0 +1,11 @@
+public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('long_description')->nullable();
+            $table->boolean('completed')->default(false);
+            $table->timestamps();
+        });
+    }
